5d4e0fe91d94d55629adc1c49238c7978aca9dd5
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-21 20:03:14 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,7 +9,7 @@ class EmailAddressParser
   def parse 
     # @emails.split(",")
  email_array = @emails.split(", ").uniq
-new_email = email_array.delete! {|email| email.empty?}
+new_email = email_array.reject! {|email| email.empty?}
  new_email
  
   end 
